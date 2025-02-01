d977dc356e4fca0f828c08cfbdc9d20c694a1375
parthmern <parthmern@gmail.com>
2025-02-01 07:45:47 -0500

    first

---
+++ apis/app/Http/Controllers/API/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    # function to create user
+    public function createUser(Request $request){
+
+        # Log::info('User Data:', $request->all());
+
+        // validation
+        $validator = Validator::make($request->all(), [
+            'name' => "required|string",
+            'email' => "required|string",
+            'password' => "required|string|min:6"
+        ]);
+
+        if($validator->fails()){
+            return response()->json(array('status'=>false, 'message' => 'validator error', 'error_message'=> $validator->error()), 400);
+        }
+
+        $user = User::create(
+            [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]
+            );
+
+        if($user->id){
+            $result = array('status'=>true, 'message' => 'user created', "data"=> $user);
+            $resCode = 201;
+        }else{
+            $result = array('status'=>false, 'message' => 'user not created');
+            $resCode = 401;
+        }
+
+        return response()->json($result, $resCode);
+    }
+}
---
+++ apis/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/test', function(Request $request){
+    return [
+        "name" => "parth",
+        "test" => "pong"
+    ];
+});
+
+# [controller::class, 'funcname']
+Route::post('/create-user', [UserController::class, 'createUser']);
